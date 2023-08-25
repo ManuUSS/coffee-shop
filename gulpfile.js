2266842b0846e754c24b9865d1e407dc8d1a0ec0
Manuel Ulate <fr3zs09@gmail.com>
2023-08-25 13:51:12 -0600

    gulp scss transpiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+const { src, dest } = require('gulp');
+const gulp_sass = require('gulp-sass')( require('sass') );
 
+const css = ( done ) => {
 
-const task = ( done ) => {
-    console.log('first');
-
+    // Identifies the file
+    src('src/scss/app.scss')
+        .pipe( gulp_sass() ) // Compiles the scss to css
+        .pipe( dest('build/css') ) //Saves the css 
     done();
 }
 
-exports.firstTask = task;
+exports.css = css;
